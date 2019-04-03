afdcfe8a77d1440e0b20a3c603cf8876054c6a50
Razerl <18813124295@163.com>
2019-04-04 00:07:44 +0800

    update inference in segmentation

--- demo/inference.py
+++ demo/inference.py
@@ -1,32 +1,90 @@
-from detection_benchmark.config import cfg
 from demo.predictor import HHODDemo
 import numpy as np
 import torch
 import cv2
 from PIL import Image
+from detection_benchmark.config import cfg
+import os
+import random
+from pycocotools.coco import COCO
 import matplotlib.pyplot as plt
 
-config_file = "../configs/e2e_faster_rcnn_R_50_C4_1x.yaml"
+config_file = "../configs/e2e_mask_rcnn_R_50_C4_1x.yaml"
 
 # update the config options with the config file
 cfg.merge_from_file(config_file)
 # manual override some options
-# cfg.merge_from_list(["MODEL.DEVICE", "cpu"])
+cfg.merge_from_list(["MODEL.DEVICE", "cpu"])
 
 hhod_demo = HHODDemo(cfg,)
 # load image and then run prediction
 
-image = Image.open('01.bmp').convert('RGB')
-img = np.asarray(image)
+# root = './samples'
+# images_depth = glob.glob(root+'/*_[a-z].bmp')
+# images = [i.replace('_d.bmp', '.bmp') for i in images_depth]
+anno_file = '/home/razer/Documents/datasets/HHOD/annotation/instances_test.json'
+root = '/home/razer/Documents/datasets/HHOD/Raw_test'
+dataset = COCO(anno_file)
 
-box = np.loadtxt('01.txt')
-box = torch.as_tensor(box).reshape(1, 4)  # guard against no boxes
+for i in range(5):
+    image_id = random.randint(24000, 31999)
+    ann_ids = dataset.getAnnIds(imgIds=image_id)
+    anno = dataset.loadAnns(ann_ids)
 
-label, score, bbox = hhod_demo.run_image(image, box)
-bbox = bbox[0]
-cv2.rectangle(img,(bbox[0],bbox[1]),(bbox[2],bbox[3]),(255,0,0),3)
-txt_center = (bbox[0],bbox[1])
-cv2.putText(img,str(label)+':'+str(float('%.4f' %score)),txt_center,cv2.FONT_HERSHEY_COMPLEX,0.5,(0,255,0),1)
-plt.imshow(img)
-plt.show()
+    path = dataset.loadImgs(image_id)[0]['file_name'].replace('.bmp', '_d.bmp')
+    img = np.array(Image.open(os.path.join(root, path)).convert('RGB'))
+    # img = np.asarray(img)
+    # img.setflags(write=1)
+    plt.figure(0)
+    plt.axis('off')
+    plt.imshow(img)
+    # box_path = image.replace('_d.bmp', '.txt')
+    # box = np.loadtxt(box_path)
+    box = [obj["bbox"] for obj in anno]
+    box = torch.as_tensor(box).reshape(1, 4)  # guard against no boxes
 
+    #show anchors
+    # target = BoxList(box,(img.shape[1],img.shape[0]), mode='xywh').convert('xyxy')
+    # target.add_field('labels',[1])
+    # targets = [target]
+    # roi_generator = make_anchor_generator(cfg)
+    # proposals = roi_generator(targets)
+    # bboxes = proposals[0].bbox.numpy()
+    # img_for_anchors = img.copy()
+    # for bbox in bboxes:
+    #     cv2.rectangle(img_for_anchors,(bbox[0],bbox[1]),(bbox[2],bbox[3]),[255,0,0],4)
+
+    # plt.figure(1)
+    # plt.imshow(img_for_anchors)
+    # plt.axis('off')
+
+    label, score, bbox, mask = hhod_demo.run_image(img, box)
+    bbox = bbox[0]
+    img_masked = hhod_demo.composite_img_with_mask(img,mask,label)
+    cv2.rectangle(img_masked, (bbox[0],bbox[1]),(bbox[2],bbox[3]),(255,0,0),3)
+    txt_center = (bbox[0], bbox[1])
+    cv2.putText(img_masked, str(label)+':'+str(float('%.4f' %score)), txt_center,cv2.FONT_HERSHEY_COMPLEX,0.5,(0,255,0),1)
+    # im_name = image.split('.')[1]+'_masked.jpg'
+    # im_name = im_name.split('/')[-1]
+    # img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+    # cv2.imwrite(im_name, img)
+    plt.figure(2)
+    plt.imshow(img_masked)
+    plt.axis('off')
+    plt.show()
+
+# data_loader = make_data_loader(
+#         cfg,
+#         is_train=True,
+#         is_distributed=False,
+#         start_iter= 0,
+#     )
+#
+# for iteration, (images, targets) in enumerate(data_loader, 0):
+#     if iteration == 1:
+#         break
+#     for i in range(8):
+#         img = targets[i].get_field('masks')[0].polygons[0].convert('mask').numpy()
+#         img[img==1] = 255
+#         img = Image.fromarray(img, 'L')
+#         img.save('mask_{}.jpg'.format(i))
--- demo/predictor.py
+++ demo/predictor.py
@@ -2,12 +2,15 @@
 import cv2
 import torch
 from torchvision import transforms as T
+import random
+import numpy as np
 
 from detection_benchmark.modeling.detector import build_detection_model
 from detection_benchmark.utils.checkpoint import DetectronCheckpointer
 from detection_benchmark.structures.image_list import to_image_list
 from detection_benchmark.data.transforms import build_transforms
 from detection_benchmark.structures.bounding_box import BoxList
+from detection_benchmark.data.datasets.evaluation import paste_mask_in_image
 
 
 class HHODDemo(object):
@@ -15,44 +18,44 @@ class HHODDemo(object):
     CATEGORIES = [
         "__background",
         "apple",
+        "bowl",
         "ball",
+        "bottle",
         "banana",
+        "box",
         "bat",
         "book",
-        "bottle",
-        "bowl",
-        "box",
-        "calculator",
         "calendar",
-        "can",
-        "carrot",
         "cucumber",
         "cup",
-        "dish",
+        "carrot",
+        "can",
+        "calculator",
         "disk",
+        "dish",
         "fan",
         "glove",
-        "handbag",
         "hat",
+        "handbag",
         "instant_noodle_bag",
-        "keyboard",
         "kiwi",
-        "medicine_bottle",
-        "melon",
-        "mobileHDD",
+        "keyboard",
         "mobilephone",
+        "melon",
         "mouse",
+        "medicine_bottle",
+        "mobileHDD",
         "neck pillow",
         "orange",
+        "paper_cup",
         "pitaya",
         "socket",
         "stapler",
-        "tissue",
-        "tomato",
         "toothpaste",
+        "tomato",
         "towel",
+        "tissue",
         "trashcan",
-        "vegetation",
         "zongzi",
     ]
 
@@ -73,9 +76,20 @@ class HHODDemo(object):
         self.transforms = build_transforms(self.cfg, is_train=False)
 
         self.cpu_device = torch.device("cpu")
+        self.labels_color = {}
+        for label in self.CATEGORIES:
+            color = [random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)]
+            self.labels_color[label] = color
+
+    def composite_img_with_mask(self, img, mask, label):
+        x, y = np.where(mask == 1)
+        color = np.array(self.labels_color[label])
+        for xi, yi in zip(x, y):
+            img[xi, yi] = color
+        return img
 
     def run_image(self, image, box):
-        target = BoxList(box, image.size, mode="xywh").convert("xyxy")
+        target = BoxList(box, image.shape[:2], mode="xywh").convert("xyxy")
         target.add_field('labels',[])
         image, target = self.transforms(image, target)
 
@@ -86,7 +100,7 @@ class HHODDemo(object):
         image_list = image_list.to(self.device)
 
         with torch.no_grad():
-            predictions = self.model(image_list,target)
+            predictions = self.model(image_list, target)
             predictions = [p.to(self.cpu_device) for p in predictions]
             prediction = predictions[0]
 
@@ -94,4 +108,6 @@ class HHODDemo(object):
         label = self.CATEGORIES[label]
         score = prediction.get_field('scores')[0].to(self.cpu_device)
         bbox = prediction.bbox
-        return label, score, bbox
+        mask = prediction.get_field('mask')
+        im_mask = paste_mask_in_image(mask[0,0],bbox[0],prediction.size[1],prediction.size[0])
+        return label, score, bbox,im_mask.numpy()
